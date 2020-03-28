60b2d02e939363876fdb90361ff6178ce9f9370c
Alexander Wunschik <alexander.wunschik@fullstax.de>
2020-03-28 21:42:34 +0100

    update raddaten

--- Radverkehrsdaten/scripts/seperateGeoJson.js
+++ Radverkehrsdaten/scripts/seperateGeoJson.js
@@ -1,21 +1,27 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const shapefile = require("shapefile");
 const encoding = require("encoding");
 
-const fileName = '../WGS84/Radverkehr_Fuerth_WGS84';
-
-shapefile.read(
-	path.resolve(__dirname, fileName + '.shp'),
-	path.resolve(__dirname, fileName + '.dbf')
-).then(geojson => {
-	console.log(geojson);
+async function genGeoJson(fileName) {
+	let geojson = await shapefile.read(
+		path.resolve(__dirname, fileName + '.shp'),
+		path.resolve(__dirname, fileName + '.dbf')
+	);
+	//console.log(geojson);
+	//geojson = encoding.convert(geojson, 'utf-8', 'windows-1252');
 	geojson = JSON.stringify(geojson, null, 2);
-	geojson = encoding.convert(geojson, 'utf-8', 'windows-1252');
-	fs.writeFileSync(path.resolve(__dirname, fileName + '.geo.json'), geojson, {
-		//encoding: 'utf-8'
-		encoding: 'latin1'
-	});
-}).catch(error => {
-	console.error(error.stack);
-});
+	await fs.outputFile(
+		path.resolve(__dirname, fileName + '.geo.json'), 
+		geojson
+	);
+};
+
+(async () => {
+	try {
+		await genGeoJson('../WGS84/Radverkehr_Fuerth_WGS84');
+		//genGeoJson('../GK4/Radverkehr_Fuerth_GK4');
+	} catch(err) {
+		console.error(err);
+	}
+})();
